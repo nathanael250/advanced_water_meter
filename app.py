4766ae19c8a47281d7b707190371c3d737bdbe46
nathanael250 <nathanaelniyogushimwa@gmail.com>
2025-04-07 22:42:12 +0200

    pushing app.py

--- app.py
+++ app.py
@@ -2583,9 +2583,12 @@ def search_users():
     
     return jsonify(results)
 
+def test():
+    pass
 
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
     app.run(debug=True, host="0.0.0.0", port=5000)
 
+#final
